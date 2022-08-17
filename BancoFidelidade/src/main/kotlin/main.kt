6dfbf88012889f0b518a24a0024e868b0f7a3c6a
brunoheenrique <bruno180396@gmail.com>
2022-08-16 21:30:28 -0300

    primeiros testes

--- BancoFidelidade/src/main/kotlin/main.kt
+++ BancoFidelidade/src/main/kotlin/main.kt
@@ -8,11 +8,24 @@ fun main (){
     println("Numero da Conta $numeroConta")
     println("Saldo da conta $saldo")
 
-   when{
-       saldo >0.0 -> println("Tem grana")
-       saldo == 0.0 -> println("Conta ta zerada")
-       else -> println("Ta no vermelho")
-   }
+    saldo = somaSaldo(saldo)
+    testaCondicao(saldo)
 
+}
 
+private fun somaSaldo(saldo: Double): Double {
+    var saldo1 = saldo
+    for (i in 1..50) {
+        saldo1 += 10 + i
+        println(saldo1)
+    }
+    return saldo1
+}
+
+private fun testaCondicao(saldo: Double) {
+    when {
+        saldo > 0.0 -> println("Tem grana")
+        saldo == 0.0 -> println("Conta ta zerada")
+        else -> println("Ta no vermelho")
+    }
 }
