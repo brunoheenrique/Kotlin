f7487ef2d645fd8f99b193b3a5def3a3f14673c9
brunoheenrique <bruno180396@gmail.com>
2022-08-16 22:57:10 -0300

    criando primeiras classes

--- BancoFidelidade/src/main/kotlin/main.kt
+++ BancoFidelidade/src/main/kotlin/main.kt
@@ -13,11 +13,40 @@ fun main() {
     contaAlex.numeroConta = 1000
     contaFran.numeroConta = 1001
 
+    println("Despositando grana")
+
+    contaAlex.depositar(4022.0)
+    contaFran.depositar(32.30)
+
+    println("Saldo do Alex ${contaAlex.saldo}")
+    println("Saldo da Fran ${contaFran.saldo}")
+
+    contaAlex.sacar(400.0)
+    contaFran.sacar(1000.0)
+
+    println("Saldo do Alex ${contaAlex.saldo}")
+    println("Saldo da Fran ${contaFran.saldo}")
+
+    contaAlex.sacar(300.40)
+    contaFran.sacar(300.00)
+
+    println("Saldo do Alex ${contaAlex.saldo}")
+    println("Saldo da Fran ${contaFran.saldo}")
+
 }
 
 class Conta {
     var titular: String = ""
     var numeroConta = 0
     var saldo = 0.0
+
+    fun depositar(valor:Double){
+        this.saldo+=valor
+    }
+
+    fun sacar(valor: Double){
+        when {this.saldo > valor -> this.saldo-=valor
+        else -> println("Valor solicitado maior do que saldo em conta. Operação cancelada")}
+    }
 }
 
