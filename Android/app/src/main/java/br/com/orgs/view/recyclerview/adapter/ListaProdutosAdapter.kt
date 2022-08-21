d4642f2c995080b81dd9c57d7af492c06843595c
brunoheenrique <bruno180396@gmail.com>
2022-08-21 15:47:42 -0300

    implementando adapter da recyclerview

--- Android/app/src/main/java/br/com/orgs/view/recyclerview/adapter/ListaProdutosAdapter.kt
+++ Android/app/src/main/java/br/com/orgs/view/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -1,19 +1,39 @@
 package br.com.orgs.view.recyclerview.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.com.orgs.R
 import br.com.orgs.model.Produto
 
 class ListaProdutosAdapter(
+    private val context: Context,
     private val produtos: List<Produto>
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    class ViewHolder (view: View): RecyclerView.ViewHolder(view) {
+        fun vincula(produto: Produto) {
+            val nome = itemView.findViewById<TextView>(R.id.main_titulo_textview)
+            val descricao = itemView.findViewById<TextView>(R.id.main_descricao_textview)
+            val valor = itemView.findViewById<TextView>(R.id.main_preco_textview)
+            nome.text = produto.nome
+            descricao.text = produto.descricao
+            valor.text = produto.valor.toPlainString()
+        }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.produto_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val produto = produtos[position]
+        holder.vincula(produto)
     }
 
     override fun getItemCount(): Int = produtos.size
