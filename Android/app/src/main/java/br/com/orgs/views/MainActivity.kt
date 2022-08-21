f70d5501e5c8b2fa46e24add7046c203c272e4fc
brunoheenrique <bruno180396@gmail.com>
2022-08-21 13:15:22 -0300

    aplicando layout simples

---
+++ Android/app/src/main/java/br/com/orgs/views/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.com.orgs.views
+
+import android.app.Activity
+import android.os.Bundle
+import br.com.orgs.R
+
+class MainActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
