30631863a4999b05fd16b4d1e4e21cbedca4d874
brunoheenrique <bruno180396@gmail.com>
2022-08-21 17:59:42 -0300

    implementando adapter da recyclerview

--- Android/app/src/main/java/br/com/orgs/view/activity/FormularioProdutoActivity.kt
+++ Android/app/src/main/java/br/com/orgs/view/activity/FormularioProdutoActivity.kt
@@ -1,9 +1,39 @@
 package br.com.orgs.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import br.com.orgs.R
+import br.com.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        val botaoSalvar =
+            findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.formulario_botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.formulario_campo_nome)
+            val campoDescricao = findViewById<EditText>(R.id.formulario_campo_descricao)
+            val campoValor = findViewById<EditText>(R.id.formulario_campo_valor)
+
+            val nome = campoNome.text.toString()
+            val descricao = campoDescricao.text.toString()
+            val valorEmTexto = campoValor.text.toString()
+            val valor = if (valorEmTexto.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valorEmTexto)
+            }
+
+            Produto(
+                nome,
+                descricao,
+                valor
+            )
+        }
+    }
 
 }
