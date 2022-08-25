2ad0d99561f1117e7de09bb2730898ec79385b0c
Bruno Henrique <bruno180396@gmail.com>
2022-08-24 22:05:02 -0300

    criando adapter da lista de personagens

--- ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/activity/CharacterListActivity.kt
+++ ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/activity/CharacterListActivity.kt
@@ -2,11 +2,17 @@ package br.com.projectrickmorty.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import br.com.projectrickmorty.R
+import br.com.projectrickmorty.view.recycler.adapter.CharacterListAdapter
 
 class CharacterListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character_list)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.character_list_recyclerview)
+        recyclerView.adapter = CharacterListAdapter()
+
     }
 }
