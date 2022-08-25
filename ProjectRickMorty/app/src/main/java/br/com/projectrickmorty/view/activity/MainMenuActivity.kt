962ff2281783f4f7ddd6a582ee49ade00ee4f2ef
Bruno Henrique <bruno180396@gmail.com>
2022-08-24 21:57:26 -0300

    criando adapter da lista de personagens

--- ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/activity/MainMenuActivity.kt
+++ ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/activity/MainMenuActivity.kt
@@ -1,12 +1,20 @@
 package br.com.projectrickmorty.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import br.com.projectrickmorty.R
 
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        val characterButton = findViewById<Button>(R.id.menu_botao_personagens)
+        characterButton.setOnClickListener{
+            val intent = Intent(this, CharacterListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
