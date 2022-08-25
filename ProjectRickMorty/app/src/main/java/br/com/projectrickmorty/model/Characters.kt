9ef7c0fcf4ed4b93d27e2b9676df29661a7f65c6
Bruno Henrique <bruno180396@gmail.com>
2022-08-24 22:38:37 -0300

    criando adapter da lista de personagens

---
+++ ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/model/Characters.kt
@@ -0,0 +1,3 @@
+package br.com.projectrickmorty.model
+
+class Characters(val name: String, val specie: String, val gender: String)
--- ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/recycler/adapter/CharacterListAdapter.kt
+++ ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/recycler/adapter/CharacterListAdapter.kt
@@ -1,19 +1,42 @@
 package br.com.projectrickmorty.view.recycler.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.com.projectrickmorty.R
+import br.com.projectrickmorty.model.Characters
+import org.w3c.dom.Text
 
-class CharacterListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+class CharacterListAdapter(
+    private val context: Context,
+    private val characters : List <Characters>
+) : RecyclerView.Adapter<CharacterListAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) :RecyclerView.ViewHolder(view) {
+
+        fun bind(character: Characters) {
+            val name = itemView.findViewById<TextView>(R.id.item_character_name)
+            name.text = character.name
+            val specie = itemView.findViewById<TextView>(R.id.item_character_specie)
+            specie.text = character.specie
+
+        }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.item_character_list, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val character = characters[position]
+        holder.bind(character)
     }
+
+    override fun getItemCount(): Int = characters.size
 
 }
