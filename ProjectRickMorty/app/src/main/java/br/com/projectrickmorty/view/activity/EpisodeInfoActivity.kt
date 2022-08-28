7332b80102a49d004c65301292ddcb891a22e6e0
Bruno Henrique <bruno180396@gmail.com>
2022-08-28 20:54:13 -0300

    implementado listas finais, e refatorando codigos

---
+++ ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/activity/EpisodeInfoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.projectrickmorty.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.projectrickmorty.R
+
+class EpisodeInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_episode_info)
+    }
+}
