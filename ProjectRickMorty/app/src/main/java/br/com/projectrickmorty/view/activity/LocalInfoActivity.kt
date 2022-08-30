838adcff737abc0d42755726d616c3dab819169c
Bruno Henrique <bruno180396@gmail.com>
2022-08-30 08:22:52 -0300

    implementado listas finais, e refatorando codigos

---
+++ ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/activity/LocalInfoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.projectrickmorty.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.projectrickmorty.R
+
+class LocalInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_local_info)
+    }
+}
