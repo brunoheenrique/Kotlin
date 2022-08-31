e7b9b54f1d6833730d0b91a9ed895dfe307643bd
Bruno Henrique <bruno180396@gmail.com>
2022-08-30 23:44:35 -0300

    implementado listas finais, e refatorando codigos

--- ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/controller/retrofit/NetworkUtils.kt
+++ ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/controller/retrofit/NetworkUtils.kt
@@ -1,19 +1,16 @@
 package br.com.projectrickmorty.controller.retrofit
 
+import br.com.projectrickmorty.controller.retrofit.services.CharacterService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class NetworkUtils {
 
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://rickandmortyapi.com/api")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-    companion object{
-
-        fun getRetrofitInstance(path:String) : Retrofit{
-            return Retrofit.Builder()
-                .baseUrl(path)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
-        }
-    }
+    val charService: CharacterService = retrofit.create(CharacterService::class.java)
 }
