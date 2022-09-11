baa7d42361c33189c3cf5b895509bd4ac0813671
Bruno Henrique <bruno180396@gmail.com>
2022-09-11 19:10:57 -0300

    aplicando testes de API, para entender como funcionar os objetos de API

--- ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/activity/CharacterInfoActivity.kt
+++ ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/activity/CharacterInfoActivity.kt
@@ -25,7 +25,7 @@ class CharacterInfoActivity : AppCompatActivity() {
         val imagemChar = findViewById<ImageView>(R.id.character_info_imageview)
         val origemTexto = findViewById<TextView>(R.id.character_info_origem)
 
-        val i = 4
+//        val i = 4
 
         viewModel.refreshCharacter(4)
         viewModel.characterByIdLiveData.observe(this) { response ->
