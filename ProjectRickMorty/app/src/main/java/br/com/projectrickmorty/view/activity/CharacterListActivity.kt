5006167419a91adc637f9bbb47f737b9e3fbe8a3
Bruno Henrique <bruno180396@gmail.com>
2022-09-15 12:42:58 -0300

    aplicando uso da API em todas as activities

--- ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/activity/CharacterListActivity.kt
+++ ProjectRickMorty/app/src/main/java/br/com/projectrickmorty/view/activity/CharacterListActivity.kt
@@ -1,6 +1,7 @@
 package br.com.projectrickmorty.view.activity
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.projectrickmorty.R
@@ -12,10 +13,28 @@ class CharacterListActivity : AppCompatActivity() {
     private var pagina: Int = 1
     private var viewModel = SharedViewModel()
     private val adapter by lazy { CharacterListAdapter(this) }
+    private val botao_proxima_pagina = findViewById<Button>(R.id.botao_charlist_proxima_pagina)
+    private val botao_pagina_anterior = findViewById<Button>(R.id.botao_charlist_pagina_anterior)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character_list)
+
+        botao_proxima_pagina.setOnClickListener {
+            pagina += 1
+            viewModel.refreshCharList(pagina)
+            viewModel.listCharacterLiveData.observe(this) {
+                adapter.setData(it)
+            }
+        }
+
+        botao_pagina_anterior.setOnClickListener {
+            pagina-=1
+            viewModel.refreshCharList(pagina)
+            viewModel.listCharacterLiveData.observe(this) {
+                adapter.setData(it)
+            }
+        }
 
         setupRecyclerView()
 
