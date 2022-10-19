977211963f6ea858c263d810dc946c01631edd3a
Bruno Henrique <bruno180396@gmail.com>
2022-10-19 12:29:03 -0300

    Iniciando novo projeto de Pokedex

--- MyPokedex/app/src/main/java/br/com/mypokedex/ui/MainActivity.kt
+++ MyPokedex/app/src/main/java/br/com/mypokedex/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.mypokedex
+package br.com.mypokedex.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.mypokedex.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ MyPokedex/app/src/main/java/br/com/mypokedex/ui/fragment/PokemonListFragment.kt
@@ -0,0 +1,19 @@
+package br.com.mypokedex.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.mypokedex.R
+
+class PokemonListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pokemon_list, container, false)
+    }
+
+}
