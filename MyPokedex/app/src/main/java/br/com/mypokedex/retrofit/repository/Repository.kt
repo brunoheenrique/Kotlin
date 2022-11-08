dae58b0253166947b1a1452e06596d48cccb21ce
Bruno Henrique <bruno180396@gmail.com>
2022-11-08 12:24:30 -0300

    criando adapter do Recycler

--- MyPokedex/app/src/main/java/br/com/mypokedex/retrofit/repository/Repository.kt
+++ MyPokedex/app/src/main/java/br/com/mypokedex/retrofit/repository/Repository.kt
@@ -1,13 +1,28 @@
 package br.com.mypokedex.retrofit.repository
 
 import br.com.mypokedex.model.PokemonList
+import br.com.mypokedex.model.PokemonPost
 import br.com.mypokedex.retrofit.NetworkUtils
 import br.com.mypokedex.retrofit.services.PokeService
 import retrofit2.Response
 
 class Repository {
 
-    suspend fun getPokemons(page:Int):Response<PokemonList>{
+    suspend fun getPokemonId(id: Int): PokemonPost? {
+        val request = NetworkUtils.pokeApiClient.getPokemonByID(id)
+
+        if (request.failed) {
+            return null
+        }
+
+        if (!request.isSuccessful) {
+            return null
+        }
+
+        return request.body
+    }
+
+    suspend fun getPokemonList(page:Int):Response<PokemonList>{
         return NetworkUtils.pokeApi.getPokemonList(page)
     }
 }
